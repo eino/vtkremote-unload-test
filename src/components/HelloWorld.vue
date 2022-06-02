4f2bf580b07c38bbc9222aea357bc44a05f48e08
Eino Gourdin <eino@extrality.ai>
2022-06-02 17:50:42 +0200

    testing unloading vtk remote view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,105 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button v-on:click="launchVisu">visualise</button>
+    <button v-on:click="closeVisu">close</button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script>
+import vtkWSLinkClient from "@kitware/vtk.js/IO/Core/WSLinkClient";
+import SmartConnect from "wslink/src/SmartConnect";
+import vtkRemoteView, {
+  connectImageStream,
+} from "@kitware/vtk.js/Rendering/Misc/RemoteView";
 
-export default defineComponent({
-  name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
+
   props: {
     msg: String,
   },
-});
+
+  methods: {
+    launchVisu() {
+      vtkWSLinkClient.setSmartConnectClass(SmartConnect);
+
+      this.divRenderer = document.createElement("div");
+      document.body.appendChild(this.divRenderer);
+
+      this.divRenderer.style.position = "relative";
+      this.divRenderer.style.width = "100vw";
+      this.divRenderer.style.height = "100vh";
+      this.divRenderer.style.overflow = "hidden";
+
+      this.remoteView = vtkRemoteView.newInstance({
+        rpcWheelEvent: "viewport.mouse.zoom.wheel",
+      });
+      this.remoteView.setContainer(this.divRenderer);
+      this.remoteView.setInteractiveRatio(0.7); // the scaled image compared to the clients view resolution
+      this.remoteView.setInteractiveQuality(50); // jpeg quality
+
+      window.addEventListener("resize", this.remoteView.resize);
+
+      this.clientToConnect = vtkWSLinkClient.newInstance();
+
+      // Error
+      this.clientToConnect.onConnectionError((httpReq) => {
+        const message =
+          (httpReq && httpReq.response && httpReq.response.error) ||
+          `Connection error`;
+        console.error(message);
+        console.log(httpReq);
+      });
+
+      // Close
+      this.clientToConnect.onConnectionClose((httpReq) => {
+        const message =
+          (httpReq && httpReq.response && httpReq.response.error) ||
+          `Connection close`;
+        console.error(message);
+        console.log(httpReq);
+      });
+
+      // hint: if you use the launcher.py and ws-proxy just leave out sessionURL
+      // (it will be provided by the launcher)
+      const config = {
+        application: "cone",
+        sessionURL: "ws://localhost:1234/ws",
+      };
+
+      // Connect
+      this.clientToConnect
+        .connect(config)
+        .then((validClient) => {
+          console.log("Client did connect");
+          connectImageStream(validClient.getConnection().getSession());
+
+          const session = validClient.getConnection().getSession();
+          this.remoteView.setSession(session);
+          this.remoteView.setViewId(-1);
+          this.remoteView.render();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+
+    closeVisu() {
+      this.remoteView?.delete();
+      this.remoteView = null;
+
+      this.clientToConnect?.disconnect(-1);
+      this.clientToConnect = null;
+
+      if (this.divRenderer) {
+        document.body.removeChild(this.divRenderer);
+        this.divRenderer = null;
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
